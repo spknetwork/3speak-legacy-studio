39bc18a0f05b0afe5b7114c00b8a8fbe484af6b3
Sagar <sagar.devices@gmail.com>
2023-06-28 16:37:07 +0530

    community selection issue fixed

--- routes/mobile/mobile-router.js
+++ routes/mobile/mobile-router.js
@@ -247,7 +247,7 @@ router.post(
       videoEntry["tags_v2"] = [];
     }
     if (typeof req.body.communityID === "string" && req.body.communityID.length > 0) {
-      videoEntry.communityID = req.body.communityID;
+      videoEntry.community = req.body.communityID;
     }
     if (typeof req.body.beneficiaries === "string" && req.body.beneficiaries.length > 0) {
       videoEntry.beneficiaries = req.body.beneficiaries;

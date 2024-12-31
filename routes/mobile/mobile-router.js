3734bf4596dbedd12646424b445ed7d4b3521fd3
Sagar Kothari <sag333ar@gmail.com>
2024-12-31 13:16:25 +0530

    correct parsing added

--- routes/mobile/mobile-router.js
+++ routes/mobile/mobile-router.js
@@ -826,9 +826,9 @@ async function pinFolderWithCluster(folderPath, clusterAPI) {
     // Log uploaded file details
     console.log(`Response from axios is as follows`);
     console.log(response.data);
-    const text = `[${response.data.split("\n").join()}]`;
-    const cidData = JSON.parse(text);
-    const folderCid = cidData[cidData.length - 1].cid;
+    const textData = response.data.split("\n").filter(a => a.length > 0);
+    const cidData = JSON.parse(textData[textData.length - 1]);
+    const folderCid = cidData.cid;
     console.log('Folder CID:', folderCid);
     return folderCid;
   } catch (err) {

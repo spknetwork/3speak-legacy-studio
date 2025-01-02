2aac42ce13622e6bc2728c6604d7250b291b6767
Sagar Kothari <sag333ar@gmail.com>
2025-01-02 14:05:27 +0530

    just change the status to published

--- routes/mobile/mobile-router.js
+++ routes/mobile/mobile-router.js
@@ -929,11 +929,7 @@ router.post(
       video.created = Date.now();
       video.upload_type = "ipfs";
       const [account] = await hive.api.getAccountsAsync([req.body.owner]);
-      if (account.posting.accountAuths[0].filter(e => e[0] === 'threespeak').length > 0) {
-        video.status = "published";
-      } else {
-        video.status = "publish_manual";
-      }
+      video.status = "published";
       video.title = req.body.title;
       video.description = req.body.description;
       video.local_filename = filePath;

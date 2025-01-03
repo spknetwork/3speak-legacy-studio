3ec895985bbe584e864475fd5e02d6ac2ec92a80
Sagar Kothari <sag333ar@gmail.com>
2025-01-03 22:32:14 +0530

    fixed error message

--- routes/mobile/mobile-router.js
+++ routes/mobile/mobile-router.js
@@ -961,7 +961,6 @@ router.post(
       return res.send(video);
     } catch (error) {
       console.error('/api/upload_zip - Error processing ZIP file:', error);
-      errorMessage = `/api/upload_zip - Error processing ZIP file: ${error.toString()}`;
       const stackLines = error.stack.split('\n');
       if (stackLines[1]) {
         console.log('Error occurred at:', stackLines[1].trim());
